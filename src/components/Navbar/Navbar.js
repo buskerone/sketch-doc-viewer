ff824b9e8356d1cb1b5706d451b0bf80b0462a86
Carlos Knopel <carlos.knopel@gmail.com>
2022-02-01 16:07:48 -0300

    feat(e2e): more e2e tests for artboard flow

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -31,13 +31,13 @@ const Navbar = ({
     {isArtboard ? (
       <>
         <div className="flex w-72 justify-start items-center">
-          <button className="focus:outline-none p-6" onClick={onClose}>
+          <button id="close-button" className="focus:outline-none p-6" onClick={onClose}>
             <img alt="close" src={CloseIcon} />
           </button>
 
           <img className="mr-2 h-10" alt="separator" src={Separator} />
 
-          <button className="focus:outline-none p-4" onClick={onPrev}>
+          <button id="prev-button" className="focus:outline-none p-4" onClick={onPrev}>
             <img alt="arrow-left" src={ArrowLeftIcon} />
           </button>
 
@@ -47,7 +47,7 @@ const Navbar = ({
 
           <span className="text-gray-500">{totalArtboards}</span>
 
-          <button className="focus:outline-none p-4" onClick={onNext}>
+          <button id="next-button" className="focus:outline-none p-4" onClick={onNext}>
             <img alt="arrow-right" src={ArrowRightIcon} />
           </button>
         </div>
--- src/e2e/artboard.test.js
+++ src/e2e/artboard.test.js
@@ -23,4 +23,37 @@ test.describe('Artboard', () => {
   test('Check if artboard image exists', async ({ page }) => {
     await page.waitForSelector('#artboard-image');
   });
+
+  test('Check if users can navigate through artboards (back)', async ({ page }) => {
+    await page.locator('#prev-button').click();
+    await expect(page).toHaveURL(
+      'http://localhost:3000/document/e981971c-ff57-46dc-a932-a60dc1804992/artboard/2'
+    );
+
+    const artboardNameElement = await page.waitForSelector('#artboard-name');
+    const artboardName = await artboardNameElement.innerText();
+    await expect(artboardName).toBe('Etch a Sketch');
+  });
+
+  test('Check if users can navigate through artboards (next)', async ({ page }) => {
+    await page.locator('#next-button').click();
+    await expect(page).toHaveURL(
+      'http://localhost:3000/document/e981971c-ff57-46dc-a932-a60dc1804992/artboard/4'
+    );
+
+    const artboardNameElement = await page.waitForSelector('#artboard-name');
+    const artboardName = await artboardNameElement.innerText();
+    await expect(artboardName).toBe('Android 4');
+  });
+
+  test('Check if users can go back to document by clicking on close button', async ({ page }) => {
+    await page.locator('#close-button').click();
+    await expect(page).toHaveURL(
+      'http://localhost:3000/document/e981971c-ff57-46dc-a932-a60dc1804992'
+    );
+
+    const documentNameElement = await page.waitForSelector('#document-name');
+    const documentName = await documentNameElement.innerText();
+    await expect(documentName).toBe('Screens');
+  });
 });
